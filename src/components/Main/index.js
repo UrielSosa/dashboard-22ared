26f685aaf6d27511e715c1341af6693d5c8c7956
Uriel Sosa <sosa.uriel1999@gmail.com>
2021-03-22 20:18:43 -0300

    add table

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Card from './Card';
 import Category from './Category';
-import profile from './assets/images/dummy-avatar.jpg';
+import Footer from './Footer';
+import Table from './Table';
+import Header from './Header';
+import Metrics from './Metrics';
+
+/* Assets */
 import dummy from './assets/images/product_dummy.svg';
+
 
 const Main = () => {
     return (
@@ -10,98 +16,12 @@ const Main = () => {
 
 			<div id="content">
 
-				<nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-					<button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
-						<i className="fa fa-bars"></i>
-					</button>
-
-					<ul className="navbar-nav ml-auto">
-
-						<li className="nav-item dropdown no-arrow mx-1">
-							<a className="nav-link dropdown-toggle" href="/" id="alertsDropdown">
-								<i className="fas fa-bell fa-fw"></i>
-								<span className="badge badge-danger badge-counter">3+</span>
-							</a>
-						</li>
-
-						<li className="nav-item dropdown no-arrow mx-1">
-							<a className="nav-link dropdown-toggle" href="/" id="messagesDropdown">
-								<i className="fas fa-envelope fa-fw"></i>
-								<span className="badge badge-danger badge-counter">7</span>
-							</a>
-						</li>
-
-						<div className="topbar-divider d-none d-sm-block"></div>
-
-						<li className="nav-item dropdown no-arrow">
-							<a className="nav-link dropdown-toggle" href="/" id="userDropdown">
-								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Walter White</span>
-								<img className="img-profile rounded-circle" src={profile} width="60" alt="profile" />
-							</a>
-						</li>
-
-					</ul>
-
-				</nav>
+				<Header />
 
 				<div className="container-fluid">
-
-					<div className="d-sm-flex align-items-center justify-content-between mb-4">
-						<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
-					</div>
-
-					<div className="row">
-
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-primary shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-primary text-uppercase mb-1"> Products in Data Base</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">135</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-success shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Amount in products</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">$546.456</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-warning shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Users quantity
-											</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">38</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-user-check fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+					<Metrics 
+						title="App dashboard"
+					/>
 
 					<div className="row">
 						{/* Cards - prueba de childrens */}
@@ -130,16 +50,11 @@ const Main = () => {
 						</Card>
 						
 					</div>
+					<Table />
 				</div>
 			</div>
 
-			<footer className="sticky-footer bg-white">
-				<div className="container my-auto">
-					<div className="copyright text-center my-auto">
-						<span>Copyright &copy; Dashboard 2020</span>
-					</div>
-				</div>
-			</footer>
+			<Footer />
 
 		</div>
     );
